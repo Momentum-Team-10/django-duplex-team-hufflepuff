f4c4ca351e2a2a086b66ba6d74d15f8f08d6d1e5
James Andrews <jbandrew14@gmail.com>
2021-11-11 17:14:08 -0500

    added Library and Comment models, started on Snippet model

--- hufflepuff/code_snips/models.py
+++ hufflepuff/code_snips/models.py
@@ -12,6 +12,24 @@ class User(AbstractUser):
         return self.username
 
 
+class Snippet(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    libraries = models.ManyToManyField('Library', related_name='snippets', blank=True)
+    languages = models.ForeignKey('Language', on_delete=models.DO_NOTHING)
+    tags = models.ManyToManyField('Tag', related_name="snippets", blank=True)
+    framework = models.ForeignKey('Framework', on_delete=models.DO_NOTHING)
+
+    def __repr__(self):
+        return f"<Snippet title={self.title}>"
+    
+    def __str__(self):
+        return self.name
+
+
+
 class Language(models.Model):
     name = models.CharField(max_length=50)
     slug = AutoSlugField(populate_from='name')
@@ -34,6 +52,16 @@ class Tag(models.Model):
         return self.name
 
 
+class Library(models.Model):
+    name = models.CharField(max_length = 75)
+    slug = AutoSlugField()
+
+    def __repr__(self):
+        return f"<Library name={self.name}>"
+
+    def __str__(self):
+        return self.name
+
 class Framework(models.Model):
     name = models.CharField(max_length=50)
     slug = AutoSlugField(populate_from='name')
@@ -43,3 +71,16 @@ class Framework(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey('User', on_delete=models.SET_NULL)
+    snippet = models.ForeignKey('Snippet', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __repr__(self):
+        return f"<Comment user={self.user}>"
+
+    def __str__(self):
+        return self.text
