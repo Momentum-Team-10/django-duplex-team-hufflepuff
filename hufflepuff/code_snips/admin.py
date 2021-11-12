d14cbcafb4e6e4b5ad166a1cee4f45a8a3b366cf
James Andrews <jbandrew14@gmail.com>
2021-11-12 08:11:59 -0500

    imported and added model registers to admin.py

--- hufflepuff/code_snips/admin.py
+++ hufflepuff/code_snips/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Snippet, Language, Tag, Library, Framework, Comment
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Snippet)
+admin.site.register(Language)
+admin.site.register(Tag)
+admin.site.register(Library)
+admin.site.register(Framework)
+admin.site.register(Comment)
