04269496d5787e64f0337648a17ac98e94aabc67
James Andrews <jbandrew14@gmail.com>
2021-11-17 14:30:05 -0500

    removed static directory inside code_snips

--- hufflepuff/code_snips/static/js/copy_function.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.getElementsByClassName("copy")
-for (let button of buttons) {
-    button.addEventListener("click", () => {
-        const parent = button.parentNode;
-        const codeBlock = parent.querySelector('code');
-        navigator.clipboard.writeText(codeBlock.innerText);
-    })
-}
