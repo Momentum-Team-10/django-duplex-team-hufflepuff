53f382895345c9ab8ba9ed35c0f59d603a89cf90
James Andrews <jbandrew14@gmail.com>
2021-11-18 10:11:33 -0500

    added login_required to all views except home_page

--- hufflepuff/code_snips/views.py
+++ hufflepuff/code_snips/views.py
@@ -18,6 +18,7 @@ def user_page(request):
   authored = Snippet.objects.filter(created_by=request.user)
   return render(request, 'code_snips/user_page.html', {"favorites": favorites, "authored": authored})
 
+@login_required
 def code_view(request, pk):
   snippet = get_object_or_404(Snippet, pk=pk)
   comments = Comment.objects.filter(snippet=snippet).order_by('-created_at')
@@ -66,16 +67,19 @@ def delete_snip(request, pk):
     return redirect('user_page')
   return render(request, 'code_snips/delete_snip.html', {'snippet': snippet})
 
+@login_required
 def filter_by_tag(request, tag):
   snippets=Snippet.objects.filter(tags__name=tag)
   snippets.order_by('-created_at')
   return render(request, 'code_snips/filtered_page.html', {"snippets":snippets, "tag":tag})
 
+@login_required
 def filter_by_language(request, language):
   snippets=Snippet.objects.filter(language__name=language)
   snippets.order_by('-created_at')
   return render(request, 'code_snips/by_language.html', {"snippets":snippets, "language":language})
 
+@login_required
 def search_by_title(request):
     # get the search term from the query params
     query = request.GET.get("q")
@@ -85,16 +89,19 @@ def search_by_title(request):
 
     return render(request, "code_snips/home.html", {"snippets": results})
 
+@login_required
 def search_by_language(request):
   query = request.GET.get("q")
   results = Snippet.objects.filter(language__name__icontains=query)
   return render(request, "code_snips/home.html", {"snippets": results})
 
+@login_required
 def search_by_tag(request):
   query = request.GET.get("q")
   results = Snippet.objects.filter(tags__name__icontains=query)
   return render(request, "code_snips/home.html", {"snippets": results})
 
+@login_required
 def search_by_user(request):
   query = request.GET.get("q")
   results = Snippet.objects.filter(created_by__username__icontains=query)
